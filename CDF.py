46fed621d47266caf86f31d741e8318b51413872
Vitor Lopes <vitorglopes@hotmail.com>
2020-07-20 11:39:53 -0300

    fixes

--- CDF.py
+++ CDF.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 class probability():
     """
     input prob=1/2, amostral_space=2 at_least=1 possibly= 3
@@ -13,11 +14,11 @@ class probability():
     def probindv(self,possibly,ntimes):
         ntimes1=possibly-ntimes
         chance=(self.prob**ntimes)*(self.prob1**ntimes1)
+        #print("passou 3")
         return chance
     def probconj(self,difchanc,possibly,ntimes):
-        chance=0
-        for x in range(difchanc):
-            chance= self.probindv(possibly,ntimes)+chance
+        chance= Decimal(self.probindv(possibly,ntimes))*Decimal(difchanc)
+        #print("passou 2")
         return chance
     def combination(self,n,p):
         a=1
@@ -27,14 +28,18 @@ class probability():
             a=x*a
         for x in range(p,0,-1):
             b=x*b
-        result= a/b
+        a=round(a,5)
+        b=round(b,5)
+        result= a//b
         return result
     def acumulate(self,atleast,possibly):
         result=0
         for x in range(atleast,possibly+1):
             difchanc=self.combination(possibly,x)
+            #print("passou 1")
             result=self.probconj(int(difchanc),possibly,x)+result
         return result
 
-cara=probability(1/2)
-print(cara.acumulate(1,5))
+cara=probability(8/2048)
+plays=50*15
+print(str(round(cara.acumulate(3,plays)*100,2))+"%" )
